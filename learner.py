84dc37fbc7693f5f4b47804ecfc52fc50770a110
Arib Dhuka <atxarib99@gmail.com>
2022-03-09 01:05:29 -0600

    Added more models and visualizations

--- learner.py
+++ learner.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import sys
+
 
 #load a single data file for now
 data = open('stocks/AG.csv', 'r')
@@ -8,7 +10,6 @@ data = open('stocks/AG.csv', 'r')
 #split data up
 #lets grab a line count
 lines = data.readlines()
-print("lines:",len(lines))
 #split train,test
 data = []
 
@@ -35,7 +36,12 @@ for i in range(1,int(len(lines))):
 
 import models
 
-model, train_x, train_y, valid_x, valid_y, test_x, test_y = models.singleHighOutput(data)
+get_model = getattr(models, sys.argv[2])
+model, train_x, train_y, valid_x, valid_y, test_x, test_y = get_model(data)
+
+#model, train_x, train_y, valid_x, valid_y, test_x, test_y = models.singleHighOutput(data, lstm_layer_size=512, lstm_layer_count=3)
+#model, train_x, train_y, valid_x, valid_y, test_x, test_y = models.next1AllOutput(data)
+#model, train_x, train_y, valid_x, valid_y, test_x, test_y = models.next5HighOutput(data, lstm_layer_size=1024, lstm_layer_count=2)
 
 def loss(labels, logits):
     return tf.keras.losses.mse(labels, logits)
@@ -44,15 +50,85 @@ model.compile(optimizer=tf.keras.optimizers.Adam(), loss=loss)
 
 print(model.summary())
 
-model.fit(train_x, train_y, epochs=50, batch_size=4)
+epochs = None
+
+try:
+    epochs = int(sys.argv[1])
+except IndexError:
+    epochs = 10
+
+history = model.fit(train_x, train_y, epochs=epochs, batch_size=4)
 
 #test_case = test_x[0]
 
 #test_case = np.reshape(test_case, (1, test_x.shape[1], test_x.shape[2]))
 
+self_error = 0
+count = 0
+
 test_predictions = model.predict(test_x)
 for i in range(0, len(test_predictions)):
-    print(test_predictions[i], '-', test_y[i])
+    self_error += abs(test_predictions[i] - test_y[i])
+    count+=1
+    #print(test_predictions[i], '-', test_y[i])
 
-#do visualizations here
+print('final error', str(self_error/count))
+print(history.history)
 
+#grab random 30 day splice
+import random as rand
+import matplotlib.pyplot as plt
+def predict5():
+    rand_start = rand.randint(0, len(data)-36)
+    prior = data[rand_start:rand_start+30]
+    prior = np.array(prior)
+    prior = np.reshape(prior, (1, 30, 6))
+
+    output = model.predict(prior)
+
+    #create plot for actual and generated data
+    actual = [row[0] for row in data[rand_start:rand_start+35]]
+
+    guessed = [row[0] for row in data[rand_start:rand_start+30]]
+    #guessed.append(guess for guess in output)
+    for guess in output[0]:
+        guessed.append(guess)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    #build test_x + its outputs
+
+    ax.plot(actual, color='tab:blue')
+    ax.plot(guessed, color='tab:orange')
+    plt.show()
+
+def predict5With1():
+    guesses = []
+    rand_start = rand.randint(0, len(data)-36)
+    for i in range(0, 5):
+        prior = data[rand_start+i:rand_start+30] 
+        #OOPS ok so the issue is that prior is already a 30 long list of 6 long lists... ie its 30x6. and then we tried to just add
+        #a single digit entry.. a 1x1 to the end which doesnt make sense. Might have to build a model with just 30x1 to see performance
+        prior.extend(guesses)
+        prior = np.array(prior)
+        prior = np.reshape(prior, (1, 30, 6))
+
+        output = model.predict(prior)
+        guesses.append(output[0])
+ 
+    actual = [row[0] for row in data[rand_start:rand_start+35]]
+
+    guessed = [row[0] for row in data[rand_start:rand_start+30]] 
+    for guess in output[0]:
+        guessed.append(guess)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    #build test_x + its outputs
+
+    ax.plot(actual, color='tab:blue')
+    ax.plot(guessed, color='tab:orange')
+    plt.show()
+
+#add logic here but for now its hard coded
+predict5With1()
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-def singleHighOutput(data):
+def singleHighOutput(data, lstm_layer_size=128, lstm_layer_count=2):
     dataset_x = []
     dataset_y = []
 
@@ -28,14 +28,102 @@ def singleHighOutput(data):
     test_x = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], 6))
     test_y = np.reshape(test_y, (test_y.shape[0], 1))
 
-    model = tf.keras.Sequential([
-        tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(30,6), kernel_initializer='glorot_uniform'),
-        tf.keras.layers.Dropout(.2),
-        tf.keras.layers.LSTM(128, return_sequences=False, kernel_initializer='glorot_uniform'),
-        tf.keras.layers.Dropout(.2),
-        tf.keras.layers.Dense(6, kernel_initializer='glorot_uniform'),
-        tf.keras.layers.Dense(1, kernel_initializer='glorot_uniform')
-    ])
+    layers = []
+
+    for i in range(0, lstm_layer_count):
+        if i != lstm_layer_count - 1:
+            layers.append(tf.keras.layers.LSTM(lstm_layer_size, return_sequences=True, input_shape=(30,6), kernel_initializer='glorot_uniform')) 
+        else:
+            layers.append(tf.keras.layers.LSTM(lstm_layer_size, return_sequences=False, input_shape=(30,6), kernel_initializer='glorot_uniform'))
+        layers.append(tf.keras.layers.Dropout(.2))
+        
+    layers.append(tf.keras.layers.Dense(6, kernel_initializer='glorot_uniform'))
+    layers.append(tf.keras.layers.Dense(1, kernel_initializer='glorot_uniform'))
+
+
+    model = tf.keras.Sequential(layers)
+
+    return (model, train_x, train_y, valid_x, valid_y, test_x, test_y)
+
+def next5HighOutput(data, lstm_layer_size=64, lstm_layer_count=4):
+    dataset_x = []
+    dataset_y = []
+
+    train_x = []
+    train_y = []
+
+    for i in range(30, len(data) - 5):
+        dataset_x.append(data[i-30:i])
+        dataset_y.append([row[0] for row in data[i+1:i+6]])
+    
+    train_x, train_y, valid_x, valid_y, test_x, test_y = splitdata(dataset_x, dataset_y, [.8,0,.2])
+
+    #reshape
+    train_x = np.array(train_x)
+    train_y = np.array(train_y)
+    train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], 6))
+    train_y = np.reshape(train_y, (train_y.shape[0], 5))
+
+    test_x = np.array(test_x)
+    test_y = np.array(test_y)
+    test_x = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], 6))
+    test_y = np.reshape(test_y, (test_y.shape[0], 5))
+
+    layers = []
+
+    for i in range(0, lstm_layer_count):
+        if i != lstm_layer_count - 1:
+            layers.append(tf.keras.layers.LSTM(lstm_layer_size, return_sequences=True, input_shape=(30,6), kernel_initializer='glorot_uniform')) 
+        else:
+            layers.append(tf.keras.layers.LSTM(lstm_layer_size, return_sequences=False, input_shape=(30,6), kernel_initializer='glorot_uniform'))
+        layers.append(tf.keras.layers.Dropout(.3))
+        
+    layers.append(tf.keras.layers.Dense(125, kernel_initializer='glorot_uniform'))
+    layers.append(tf.keras.layers.Dense(5, kernel_initializer='glorot_uniform'))
+
+
+    model = tf.keras.Sequential(layers)
+
+    return (model, train_x, train_y, valid_x, valid_y, test_x, test_y)
+
+def next1AllOutput(data, lstm_layer_size=256, lstm_layer_count=3):
+    dataset_x = []
+    dataset_y = []
+
+    train_x = []
+    train_y = []
+
+    for i in range(30, len(data) - 1):
+        dataset_x.append(data[i-30:i])
+        dataset_y.append(data[i+1])
+    
+    train_x, train_y, valid_x, valid_y, test_x, test_y = splitdata(dataset_x, dataset_y, [.8,0,.2])
+
+    #reshape
+    train_x = np.array(train_x)
+    train_y = np.array(train_y)
+    train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], 6))
+    train_y = np.reshape(train_y, (train_y.shape[0], 6))
+
+    test_x = np.array(test_x)
+    test_y = np.array(test_y)
+    test_x = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], 6))
+    test_y = np.reshape(test_y, (test_y.shape[0], 6))
+
+    layers = []
+
+    for i in range(0, lstm_layer_count):
+        if i != lstm_layer_count - 1:
+            layers.append(tf.keras.layers.LSTM(lstm_layer_size, return_sequences=True, input_shape=(30,6), kernel_initializer='glorot_uniform')) 
+        else:
+            layers.append(tf.keras.layers.LSTM(lstm_layer_size, return_sequences=False, input_shape=(30,6), kernel_initializer='glorot_uniform'))
+        layers.append(tf.keras.layers.Dropout(.2))
+        
+    layers.append(tf.keras.layers.Dense(6, kernel_initializer='glorot_uniform'))
+    layers.append(tf.keras.layers.Dense(6, kernel_initializer='glorot_uniform'))
+
+
+    model = tf.keras.Sequential(layers)
 
     return (model, train_x, train_y, valid_x, valid_y, test_x, test_y)
 
